aa208260fe8126302318bdf31efb4937f4091e2c
Salar <ssalar8998@gmail.com>
2022-01-25 12:51:04 -0600

    initial

---
+++ src/test/java/com/StudentMS/StudentMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.StudentMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
