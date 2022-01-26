c3b35704f9e81e6571354af83cf29380d40a3363
Salar <ssalar8998@gmail.com>
2022-01-26 09:16:51 -0600

    added course dao and model

--- src/main/java/com/StudentMS/dao/CourseDao.java
+++ src/main/java/com/StudentMS/dao/CourseDao.java
@@ -4,10 +4,22 @@
  */
 package com.StudentMS.dao;
 
+import com.StudentMS.models.Course;
+import com.StudentMS.models.Student;
+import com.StudentMS.models.Teacher;
+import java.util.List;
+
 /**
  *
  * @author ssalar
  */
 public interface CourseDao {
+    Course getCourseById(int id);
+    List<Course> getAllCourses();
+    Course addCourse(Course course);
+    void updateCourse(Course course);
+    void deleteCourseById(int id);
     
+    List<Course> getCoursesForTeacher(Teacher teacher);
+    List<Course> getCoursesForStudent(Student student);
 }
--- src/main/java/com/StudentMS/models/Course.java
+++ src/main/java/com/StudentMS/models/Course.java
@@ -4,10 +4,70 @@
  */
 package com.StudentMS.models;
 
+import java.util.List;
+
 /**
  *
  * @author ssalar
  */
 public class Course {
+    private int id;
+    private String name;
+    private String description;
+    private Teacher teacher;
+    private List<Student> students;
+
+    public Course(int id, String name, String description, Teacher teacher, List<Student> students) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.teacher = teacher;
+        this.students = students;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Teacher getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+    
+    
+    
+    
+    
     
 }
