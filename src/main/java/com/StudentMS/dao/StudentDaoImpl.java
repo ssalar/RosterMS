80122da013e20c1bf541164a6107c16c9209e7ab
pujitham99 <pujithamandava99@gmail.com>
2022-01-26 15:53:33 -0600

    updated StudentDaoImpl_pujitha

--- src/main/java/com/StudentMS/dao/StudentDaoImpl.java
+++ src/main/java/com/StudentMS/dao/StudentDaoImpl.java
@@ -1,5 +1,51 @@
 package com.StudentMS.dao;
 
-public class StudentDaoImpl {
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.StudentMS.models.Student;
+
+@Repository
+public class StudentDaoImpl implements StudentDao {
+	List<Student> students = new ArrayList<>();
+
+	@Override
+	public Student addStudent(Student student) {
+		Student newstudent = new Student(student.getId(),student.getFirstName(),student.getLastName());
+		students.add(newstudent);
+		return newstudent;
+	}
+
+	@Override
+	public Student getStudentById(int id) {
+		for (Student student : students){
+            if (student.getId() == id){
+                return student;
+            }
+        }
+        
+        return null;
+	}
+
+	@Override
+	public List<Student> listStudents() {
+		
+		return students;
+	}
+
+	@Override
+	public int updateStudentById() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteStudentById() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
+
