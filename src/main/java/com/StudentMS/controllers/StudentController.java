ac7912b3035c5eb856a845d5a16e0de0ab6bcf52
Salar <ssalar8998@gmail.com>
2022-01-27 15:14:15 -0600

    delete api

--- src/main/java/com/StudentMS/controllers/StudentController.java
+++ src/main/java/com/StudentMS/controllers/StudentController.java
@@ -8,34 +8,51 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.StudentMS.dao.StudentDao;
+import com.StudentMS.dao.StudentDaoImpl;
 import com.StudentMS.models.Student;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("api/Student")
 public class StudentController {
-	private final StudentDao studentDao;
+    
+    private  StudentDaoImpl studentDaoImpl;
 	
-	@Autowired
-	public StudentController (StudentDao studentdao) {
-		this.studentDao = studentdao;
+        @Autowired
+	public StudentController(StudentDaoImpl studentDaoImpl) {
+		super();
+		this.studentDaoImpl = studentDaoImpl;
+	}
+	
+	@PostMapping
+	public Student insertStudent(@RequestBody Student student) {
+		return studentDaoImpl.addStudent(student);
+	}
+	
+	@GetMapping
+	public List<Student> getStudents() {
+		return studentDaoImpl.listStudents();
 		
 	}
-	@PostMapping	
-		public void addStudent(Student student) {
-			studentDao.addStudent(student);
-			
-		}
-//	@GetMapping	
-//		public void getStudentById(int id) {
-//			studentDao.getStudentById(id);
-//			
-//			
-//		}
-//	@GetMapping
-//		public void List() {
-//			
-//		}
+	@GetMapping(path = "{id}")
+	public Student getStudentById(@PathVariable("id") int id) {
+		
+		return studentDaoImpl.getStudentById(id);
+		
+		
 	}
+	@DeleteMapping(path = "{id}")
+	public int deleteStudentById(@PathVariable("id") int id) {
+		return studentDaoImpl.deleteStudentById(id);
+		
+	}
+	@PutMapping
+	public int updateStudents(@RequestBody Student student) {
+		return studentDaoImpl.updateStudent(student);
+	}
+}
 
 
